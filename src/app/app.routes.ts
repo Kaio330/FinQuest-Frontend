1d747100365684218d8f640b1c3ead73d683d9e1
KAIO MELUGO VIEIRA <kaio.vieira01@fatec.sp.gov.br>
2026-05-19 19:53:51 -0300

    Adicionando a roda da tela inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { DashboardComponent } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
     {
         path: "login",
+        component: Login
+    },
+    {
+        path: "inicio",
+        component: DashboardComponent
+    },
+    {
+        path: "",
         component: Login
     }
 ];
